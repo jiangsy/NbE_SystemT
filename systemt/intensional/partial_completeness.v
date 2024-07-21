2bdd476be4870f398acd6e76005a8b07bd686271
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-07-21 21:27:41 +0800

    bot_subset_T_subset_top

--- systemt/intensional/partial_completeness.v
+++ systemt/intensional/partial_completeness.v
@@ -50,12 +50,57 @@ Combined Scheme d_dne_mutind from d_ind, dne_ind. *)
 
 Lemma bot_subset_T_subset_top : forall T,
    (forall e, e ∈ ⊥ -> ⟦ T ⟧T (d_ne e)) /\
-   (forall d, ⟦ T ⟧T d -> d ∈ ⊤ ).
+   (forall d, ⟦ T ⟧T d -> d ∈ ⊤).
 Proof with eauto using SemNat, RNeRel, RNfRel.
    intros. induction T; split; try simpl...
    - intros. dependent induction H.
      + unfold Top. intros...
-     + admit.
-     + admit.
-   - intros. admit.
-Admitted.
+     + unfold Top in *.
+       intros. specialize (IHSemNat n).
+       destruct IHSemNat as [v]...
+     + unfold Top in *. unfold Bot in *.
+       intros. specialize (H n).
+       destruct H as [u]...
+   - intros.
+     unfold Bot in *. 
+     unfold SemArr. intros.
+     exists (d_ne (dn_app e a)). split.
+     + destruct IHT1. 
+       destruct IHT2.
+       apply H3. intros.
+       apply H2 in H0. unfold Top in H0.
+       specialize (H n).
+       specialize (H0 n).
+       destruct H as [u]. destruct H0 as [v]...
+     + econstructor.
+   - intros. unfold SemArr in *.
+     unfold Top in *. intros. 
+     destruct IHT1.
+     destruct IHT2.
+     specialize (H (d_ne (dn_l n))).
+     assert ((⟦ T1 ⟧T) (d_ne (dn_l n))). {
+       eapply H0. unfold Bot. intros...
+     }
+     apply H in H4. destruct H4 as [b]. destruct H4 as [Hb Happ].
+     dependent destruction Happ.
+     + eapply H3 with (n:=S n) in Hb. 
+       destruct Hb as [v]. exists (nf_abs v). 
+       eapply rnf_abs; eauto.
+     + eapply H3 with (n:=n) in Hb.
+       destruct Hb as [v]. dependent destruction H4.
+       dependent destruction H4...
+Qed.
+
+Corollary bot_subset_T : forall e T, 
+  e ∈ ⊥ -> 
+  ⟦ T ⟧T (d_ne e).
+Proof.
+  specialize bot_subset_T_subset_top. intros. specialize (H T). intuition. 
+Qed.
+
+Corollary T_subset_top : forall d T, 
+  ⟦ T ⟧T d -> d ∈ ⊤.
+Proof.
+  specialize bot_subset_T_subset_top. intros. specialize (H T). intuition. 
+Qed.
+  
