ef42a62ce4dcff17a34413eaf23d147004990cb9
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-07-24 16:11:14 +0800

    look at sem typing of PER

--- systemt/extensional/per.v
+++ systemt/extensional/per.v
@@ -50,6 +50,36 @@ Proof.
   exists (nf_ne u). intuition.
 Qed.
 
+Lemma sem_bot_rec : forall e e' dz dz' ds ds' T,
+  e ≈ e' ∈ ⊥ ->
+  dz ≈ dz' ∈ ⊤ ->
+  ds ≈ ds' ∈ ⊤ ->
+  dne_rec T dz ds e ≈ dne_rec T dz' ds' e' ∈ ⊥.
+Proof.
+  intros. unfold SemTypBot in *. unfold SemTypTop in *.
+  intros. specialize (H n). specialize (H0 n). specialize (H1 n).
+  destruct H as [u [Hrnee Hrnee']].
+  destruct H0 as [wz [Hrnfdz Hrnfdz']].
+  destruct H1 as [ws [Hrnfds Hrnfds']].
+  eauto.
+Qed.
+
+Lemma sem_top_zero : 
+  (dnf_reif ℕ d_zero) ≈ (dnf_reif ℕ d_zero) ∈ ⊤.
+Proof.
+  unfold SemTypTop. intros; eauto.
+Qed.
+
+Lemma sem_top_suc : forall a a',
+  (dnf_reif ℕ a) ≈ (dnf_reif ℕ a') ∈ ⊤ ->
+  (dnf_reif ℕ (d_suc a)) ≈ (dnf_reif ℕ (d_suc a')) ∈ ⊤.
+Proof.
+  intros. unfold SemTypTop in *. intros.
+  specialize (H n).
+  destruct H as [w [Hrnfa Hrnfa']].
+  eauto.
+Qed.
+
 Lemma sem_typ_top_abs : forall f f' S T,
   (forall e e', e ≈ e' ∈ ⊥ -> 
     exists b b', f ∙ (d_refl S e) ↘ b /\ f' ∙ (d_refl S e') ↘ b' /\ 
@@ -62,4 +92,32 @@ Proof.
   destruct H0 as [b [b']]. intuition.
   unfold SemTypTop in H3. specialize (H3 (1 + n)). destruct H3 as [w].
   exists (nf_abs w); intuition; eauto.
+Qed.
+
+Definition Realize (T : Typ) (A : SemTyp) : Prop :=
+  (forall a a', A a a' -> dnf_reif T a ≈ dnf_reif T a' ∈ ⊤) /\
+  (forall e e', e ≈ e' ∈ ⊥ -> A (d_refl T e) (d_refl T e')).
+
+Notation " T ⊩ A " := (Realize T A)
+  (at level 55, no associativity).
+
+Inductive SemTypNat : D -> D -> Prop :=
+  | snat_zero: SemTypNat d_zero d_zero
+  | snat_suc : forall a a',
+      SemTypNat a a' ->
+      SemTypNat (d_suc a) (d_suc a')
+  | snat_ne : forall e e',
+      e ≈ e' ∈ ⊥ ->
+      SemTypNat (d_refl ℕ e) (d_refl ℕ e').
+
+Lemma nat_realize_sem_nat : ℕ ⊩ SemTypNat.
+Proof.
+  unfold Realize. split; intros.
+  - induction H; unfold SemTypTop; intros; eauto.
+    + unfold SemTypTop in IHSemTypNat. specialize (IHSemTypNat n).
+      destruct IHSemTypNat as [w [Hrnfa Hrnfa']].
+      eauto.
+    + unfold SemTypBot in H. specialize (H n).
+      destruct H as [u]. intuition. eauto.
+  - constructor; auto.
 Qed.
