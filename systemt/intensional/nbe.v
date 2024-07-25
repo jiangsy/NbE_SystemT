95369222caee65913cb9a46d82a3822d7794d57b
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-07-25 14:41:48 +0800

    rm unused files

--- systemt/intensional/nbe.v
+++
@@ -1,8 +0,0 @@
-Require Import nbe.systemt.intensional.syntax.
-Require Import nbe.systemt.intensional.semantic.
-
-Definition init_env (n : nat) : Env :=
-  fun i => d_ne (dn_l (n - i - 1)).
-
-Definition nbe (n : nat) (t : Exp) (w : Nf) : Prop := 
-  exists d, ⟦ t ⟧ (init_env n) ↘ d /\ Rⁿᶠ ⦇ n ⦈ d ↘ w.
