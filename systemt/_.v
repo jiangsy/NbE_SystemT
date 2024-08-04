395718a482c62bfc8f63b87251798caa7913f9dd
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-08-05 00:13:44 +0800

    use section to avoid notation conflict

--- systemt/_.v
+++ systemt/_.v
@@ -1,12 +1,24 @@
-(* Soundness of the syntactic rules (typing and equality) *)
-Require Import nbe.systemt.intensional.totality.
+Require nbe.systemt.intensional.totality.
+Require nbe.systemt.extensional.soundness.
+Require nbe.systemt.extensional.completeness.
 
-Print Assumptions nbe.systemt.intensional.totality.nbe_totality.
+Section Intensional.
 
-Require Import nbe.systemt.extensional.soundness.
-Require Import nbe.systemt.extensional.completeness.
+    Import nbe.systemt.intensional.totality.
 
-Print Assumptions nbe.systemt.extensional.soundness.nbe_soundness.
-Print Assumptions nbe.systemt.extensional.soundness.nbe_totality.
-Print Assumptions nbe.systemt.extensional.completeness.nbe_completeness.
-Print Assumptions nbe.systemt.extensional.completeness.nbe_totality.
+    Print Assumptions nbe.systemt.intensional.totality.nbe_totality.
+
+End Intensional.
+
+
+Section Extensional.
+
+    Import nbe.systemt.extensional.soundness.
+    Import nbe.systemt.extensional.completeness.
+
+    Print Assumptions nbe.systemt.extensional.soundness.nbe_soundness.
+    Print Assumptions nbe.systemt.extensional.soundness.nbe_totality.
+    Print Assumptions nbe.systemt.extensional.completeness.nbe_completeness.
+    Print Assumptions nbe.systemt.extensional.completeness.nbe_totality.
+    
+End Extensional.
